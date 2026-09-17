da833de0d21e46eecf7f7a437defe9b71a07b9a7
Hanako Ikezawa <anuarali@fel.cvut.cz>
2026-09-17 22:05:39 +0200

    some technical files were added by mistake

--- evaluate_molecular_egfr.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Backward-compatible source-tree wrapper for ``archcon-evaluate-egfr``."""
-
-from archcon.evaluate_egfr import main
-
-if __name__ == "__main__":
-    main()
-
